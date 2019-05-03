e92545da27616990cb822af0a70aebe1e82e21cb
Nlipski <13ml76@queensu.ca>
2019-05-03 15:05:13 -0400

    getting static background
    getting 200 frames and average them to acquire a static background for
    further removal

--- main.py
+++ main.py
@@ -1,54 +1,78 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 def nothing(x):
     pass
 
+def get_background():
+
+	cap = cv2.VideoCapture(0)
+
+	ret,frame = cap.read()
+	
+	i = 0
+	mean = frame
+	temp = np.float32(frame)
+
+	while (i < 200) :
+		ret,frame = cap.read()
+		cv2.accumulate(frame,mean)
+		i += 1
+	mean = mean /200.0
+	return mean
+
+
+
+
 
 def __main__():
-	cap = cv.VideoCapture(0)
+	cap = cv2.VideoCapture(0)
 
 
-	fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
-	cv.namedWindow('edges')
+	fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+	cv2.namedWindow('edges')
 
-	cv.createTrackbar('maxVal','edges',0,255, nothing)
-	cv.createTrackbar('minVal','edges',0,255, nothing)
-	
+	cv2.createTrackbar('maxVal','edges',0,255, nothing)
+	cv2.createTrackbar('minVal','edges',0,255, nothing)
+
+	background = get_background()
+
+	cv2.imshow('background',background)
+
 	while(True):
 
 		ret,frame = cap.read()
 
     	# Our operations on the frame come here
 		
-		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		
-		cv.imshow('gray',gray)
+		#cv.imshow('gray',gray)
 
-		filtered = cv.bilateralFilter(gray,9,75,75)
+		filtered = cv2.bilateralFilter(gray,9,75,75)
 
-		cv.imshow('filtered',filtered)
+		#cv.imshow('filtered',filtered)
 		fgmask = fgbg.apply(frame)
 		
-		cv.imshow('subtracted',fgmask)
+		#cv.imshow('subtracted',fgmask)
 
-		maxVal = cv.getTrackbarPos('maxVal','edges')
-		minVal = cv.getTrackbarPos('minVal','edges')
+		maxVal = cv2.getTrackbarPos('maxVal','edges')
+		minVal = cv2.getTrackbarPos('minVal','edges')
 
 		if maxVal <  minVal:
 			minVal = maxVal
-		ret, thresh = cv.threshold(fgmask, minVal, maxVal, 0)
-		cv.imshow('thresh',thresh)
+		ret, thresh = cv2.threshold(fgmask, minVal, maxVal, 0)
+		cv2.imshow('thresh',thresh)
 
 		
 		
 
-		edges = cv.Canny(thresh,minVal,maxVal)
-		cv.imshow('edges',edges)
+		edges = cv2.Canny(thresh,minVal,maxVal)
+		#cv.imshow('edges',edges)
 
 
-		contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		#print (len(contours[0]))
 		if len(contours) > 0:
 		#	cv.drawContours(frame, contours,0, (0,255,0), 3)
@@ -56,18 +80,18 @@ def __main__():
 		#if len(contours) > 1:
 		#	cv.drawContours(frame, contours,1, (0,0,255), 3)
 		#if len(contours) > 25:
-			cv.drawContours(frame, contours,len(contours) -1, (255,0, 0), 3)
+			cv2.drawContours(frame, contours,len(contours) -1, (255,0, 0), 3)
 		
-		cv.imshow('contour',frame)
+		#cv.imshow('contour',frame)
 
 
 		#print (hierarchy)
-		if cv.waitKey(1) & 0xFF == ord('q'):
+		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
 	# When everything done, release the capture
 	cap.release()
-	cv.destroyAllWindows()
+	cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
