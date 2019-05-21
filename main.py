2bd61c0d94ecc6fee74ff8fc52afb4864867605a
Nlipski <13ml76@queensu.ca>
2019-05-21 09:46:49 -0400

    Clean up and refactor
    cleanup the code for further proper implementation

--- main.py
+++ main.py
@@ -5,87 +5,51 @@ from matplotlib import pyplot as plt
 def nothing(x):
     pass
 
-def get_background():
-
-	cap = cv2.VideoCapture(0)
-
-	ret,frame = cap.read()
+def detect_edges(frame, minVal, maxVal):
 	
-	i = 0
-	mean = frame
-	temp = np.float32(frame)
+	result = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	while (i < 200) :
-		ret,frame = cap.read()
-		cv2.accumulate(frame,mean)
-		i += 1
-	mean = mean /200.0
-	return mean
+	#result = cv2.bilateralFilter(result,9,75,75)
+	result = cv2.GaussianBlur(result,(5,5),0)
+	# use Gaussian or bilaterak filter for smoothing the image 
+	# Gaussian is faster, but it doesn't preserve edges as well as bilateral
+
+	result = cv2.Canny(result,minVal,maxVal)
+	# edges below minVal are rejected,
+	# the ones above maxVal are added, 
+	# and the ones are between are added only if they're adjusted to strong adges
+	return result
 
 
+def main():
 
-
-
-def __main__():
 	cap = cv2.VideoCapture(0)
-
-
-	fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+	
+	cap.set(3,640) #width=640
+	cap.set(4,480) #height=480
 	cv2.namedWindow('edges')
-
 	cv2.createTrackbar('maxVal','edges',0,255, nothing)
 	cv2.createTrackbar('minVal','edges',0,255, nothing)
-
-	background = get_background()
-
-	cv2.imshow('background',background)
 
 	while(True):
 
 		ret,frame = cap.read()
-
-    	# Our operations on the frame come here
-		
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		
-		#cv.imshow('gray',gray)
-
-		filtered = cv2.bilateralFilter(gray,9,75,75)
-
-		#cv.imshow('filtered',filtered)
-		fgmask = fgbg.apply(frame)
-		
-		#cv.imshow('subtracted',fgmask)
+		if ret == 0:
+			break
 
 		maxVal = cv2.getTrackbarPos('maxVal','edges')
 		minVal = cv2.getTrackbarPos('minVal','edges')
-
+		
 		if maxVal <  minVal:
 			minVal = maxVal
-		ret, thresh = cv2.threshold(fgmask, minVal, maxVal, 0)
-		cv2.imshow('thresh',thresh)
 
-		
-		
+		#ret, thresh = cv2.threshold(fgmask, minVal, maxVal, 0)
 
-		edges = cv2.Canny(thresh,minVal,maxVal)
-		#cv.imshow('edges',edges)
+		edges = detect_edges(frame, minVal, maxVal)
 
+		cv2.imshow('edges', edges)
+		plt.imshow('edges_plot', edges)
 
-		contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		#print (len(contours[0]))
-		if len(contours) > 0:
-		#	cv.drawContours(frame, contours,0, (0,255,0), 3)
-		#	print (len(contours[0]))
-		#if len(contours) > 1:
-		#	cv.drawContours(frame, contours,1, (0,0,255), 3)
-		#if len(contours) > 25:
-			cv2.drawContours(frame, contours,len(contours) -1, (255,0, 0), 3)
-		
-		#cv.imshow('contour',frame)
-
-
-		#print (hierarchy)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
@@ -95,4 +59,9 @@ def __main__():
 
 
 if __name__ == "__main__":
-	__main__()
+	try:
+		main()
+	except:
+        #print_time_report()
+        #finish_noise(error = True)
+		raise()
