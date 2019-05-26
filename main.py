1cb22a60f7e882e4b67a22838677b7d0971c3add
Nlipski <13ml76@queensu.ca>
2019-05-26 09:08:17 -0400

    Finding body contour
    Finding the contours from thresholded image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 from matplotlib import pyplot as plt
 from skimage.measure import compare_ssim
 
@@ -18,8 +19,8 @@ def prep_image(frame):
 
 def detect_edges(frame, minVal, maxVal):
 
-	frame = cv2.medianBlur(frame,5)
-	result = cv2.Canny(frame,minVal,maxVal)
+	frame = cv2.medianBlur(frame, 5)
+	result = cv2.Canny(frame, minVal, maxVal)
 	# edges below minVal are rejected,
 	# the ones above maxVal are added, 
 	# and the ones are between are added only if they're adjusted to strong adges
@@ -43,10 +44,8 @@ def process_image(cap):
 		frame = prep_image(orig_frame)
 		fgmask = fgbg.apply(frame)
 		
-		# diff to see that nothing has been moving 
-		retval = cv2.countNonZero(fgmask)
-
-		if retval == 0:
+		# diff to see that nothing has been moving  
+		if (cv2.countNonZero(fgmask)) == 0:
 			return orig_frame
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -55,9 +54,9 @@ def process_image(cap):
 def process_stream(cap, background):
 	
 	cv2.namedWindow('edges')
-	cv2.createTrackbar('maxVal','edges',150,255, nothing)
-	cv2.createTrackbar('minVal','edges',50,255, nothing)
-	cv2.createTrackbar('thresh','edges',0,255, nothing)
+	cv2.createTrackbar('maxVal','edges',180,255, nothing)
+	cv2.createTrackbar('minVal','edges',30,255, nothing)
+	cv2.createTrackbar('thresh','edges',1,255, nothing)
 	while(True):
 
 		ret,frame = cap.read()
@@ -71,9 +70,20 @@ def process_stream(cap, background):
 			minVal = maxVal
 
 		frame = background_extraction(frame, background,thresh)
-		edges = detect_edges(frame, minVal, maxVal)
+		#edges = detect_edges(frame, minVal, maxVal)
+		contours,__ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		result = np.zeros_like(frame)
+		if len(contours) != 0:
+			max_contour = contours[0]
+			for contour in contours:
+				if cv2.contourArea(contour) > cv2.contourArea(max_contour):
+					max_contour = contour
+			#print (contours)
+			if cv2.contourArea(max_contour) >= 3000:
+					cv2.drawContours(result, max_contour , -1, (255,50,255), 3)
 
-		cv2.imshow('edges', edges)
+		cv2.imshow('result', result)
+		cv2.imshow('edges', result)
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
@@ -85,7 +95,7 @@ def background_extraction(frame, background, thresh):
 	ret, result = compare_ssim(frame, background, full=True)
 	result = (result * 255).astype("uint8")
 
-	ret,thresh_img = cv2.threshold(result, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+	ret,thresh_img = cv2.threshold(result, thresh, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
 	return thresh_img 
 
