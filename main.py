7a6b2cf820624bab1c598f4eba96b88da59fdf08
Nlipski <13ml76@queensu.ca>
2019-06-19 08:51:18 -0400

    Improve contour detection
    Added morphological extraction with 2x2 kernel
    Removed edge detection

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def process_image(cap):
 
 	fgbg = cv2.createBackgroundSubtractorMOG2()
 	count = 0
+	print("Beginning to take background image")
 	while (True):
 
 		ret, orig_frame = cap.read()
@@ -40,14 +41,15 @@ def process_image(cap):
 		if count < 60:
 			count += 1
 			continue
-
+		print("Camera warm up complete")
 		frame = prep_image(orig_frame)
 		fgmask = fgbg.apply(frame)
 		
 		# diff to see that nothing has been moving  
 		if (cv2.countNonZero(fgmask)) == 0:
+			print("Background capture is complete")
 			return orig_frame
-
+		print("Something is moving! Can't take background image.\nRepeating the process. ")
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
@@ -57,6 +59,11 @@ def process_stream(cap, background):
 	cv2.createTrackbar('maxVal','edges',180,255, nothing)
 	cv2.createTrackbar('minVal','edges',30,255, nothing)
 	cv2.createTrackbar('thresh','edges',1,255, nothing)
+	
+	kernel = np.ones((2,2),np.uint8)
+
+	print("Beginning to capture the stream")
+
 	while(True):
 
 		ret,frame = cap.read()
@@ -66,24 +73,34 @@ def process_stream(cap, background):
 		maxVal = cv2.getTrackbarPos('maxVal','edges')
 		minVal = cv2.getTrackbarPos('minVal','edges')
 		thresh = cv2.getTrackbarPos('thresh','edges')
+
 		if maxVal <  minVal:
 			minVal = maxVal
 
 		frame = background_extraction(frame, background,thresh)
-		#edges = detect_edges(frame, minVal, maxVal)
-		contours,__ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		closing = cv2.morphologyEx(frame1, cv2.MORPH_OPEN, kernel)
+		
+		contours,__ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		result = np.zeros_like(frame)
+		cutout = np.zeros_like(frame)
+		
 		if len(contours) != 0:
 			max_contour = contours[0]
 			for contour in contours:
 				if cv2.contourArea(contour) > cv2.contourArea(max_contour):
 					max_contour = contour
-			#print (contours)
+
+			contours_poly = cv2.approxPolyDP(max_contour, 3, True)
+			boundRect = cv2.boundingRect(contours_poly)
+			moment = cv2.moments(max_contour)
+			
 			if cv2.contourArea(max_contour) >= 3000:
-					cv2.drawContours(result, max_contour , -1, (255,50,255), 3)
+				print ("stream: Found contour with area: " + str(cv2.contourArea(max_contour)))
+				cv2.drawContours(result, max_contour , -1, (255,0,255))
+				cv2.rectangle(result, (int(boundRect[0]), int(boundRect[1])),(int(boundRect[0]+boundRect[2]), int(boundRect[1]+boundRect[3])), (70,150,50), 2)
 
 		cv2.imshow('result', result)
-		cv2.imshow('edges', result)
+		cv2.imshow('extracted', closing)
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
